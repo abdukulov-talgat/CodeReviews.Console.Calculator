1ad7c2b767f771ad137c4025045a0e69e3710d1c
abdukulov-talgat <abdukulov.talgat@gmail.com>
2026-09-24 11:01:09 +0500

    Limit output fractional part by 2 symbols

--- abdukulov-talgat.Calculator/CalculatorLibrary/Operation.cs
+++ abdukulov-talgat.Calculator/CalculatorLibrary/Operation.cs
@@ -2,18 +2,20 @@
 
 public record Operation(double Left, double Right, OperationType OperationType, double Result)
 {
+    
+    // I have no idea what it means 10x. I don't think it means 10 * x 
     public override string ToString()
     {
         return OperationType switch
         {
-            OperationType.Add => $"{Left} + {Right} = {Result:0.####}",
-            OperationType.Subtract => $"{Left} - {Right} = {Result:0.####}",
-            OperationType.Multiply => $"{Left} * {Right} = {Result:0.####}",
-            OperationType.Division => $"{Left} / {Right} = {Result:0.####}",
-            OperationType.Pow => $"{Left} ^ {Right} = {Result:0.####}",
-            OperationType.Sqrt => $"sqrt({Left}) = {Result:0.####}",
-            OperationType.Sin => $"{Left} sin = {Result:0.####}",
-            OperationType.Cos => $"{Left} cos = {Result:0.####}",
+            OperationType.Add => $"{Left:0.##} + {Right:0.##} = {Result:0.##}",
+            OperationType.Subtract => $"{Left:0.##} - {Right:0.##} = {Result:0.##}",
+            OperationType.Multiply => $"{Left:0.##} * {Right:0.##} = {Result:0.##}",
+            OperationType.Division => $"{Left:0.##} / {Right:0.##} = {Result:0.##}",
+            OperationType.Pow => $"{Left:0.##} ^ {Right:0.##} = {Result:0.##}",
+            OperationType.Sqrt => $"sqrt({Left:0.##}) = {Result:0.##}",
+            OperationType.Sin => $"sin {Left:0.##} = {Result:0.##}",
+            OperationType.Cos => $"cos {Left:0.##} = {Result:0.##}",
             _ => throw new ArgumentOutOfRangeException(nameof(OperationType), OperationType, null),
         };
     }
